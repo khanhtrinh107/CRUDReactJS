5a0bd05cabb7b0b650792b24cefe1351d8cfcfd6
khanhtrinh107 <khanhtrinh107@gmail.com>
2023-06-25 15:16:32 +0700

    'zz'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Books from './components/Books';
-import Book from './components/Book';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Books from "./components/Books";
+import Book from "./components/Book";
 
 function App() {
+  //stahs
+  //oke32
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Books/>}/>
-        <Route path='books/:bookcode' element={<Book/>}/>
+        <Route path="/" element={<Books />} />
+        <Route path="books/:bookcode" element={<Book />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
